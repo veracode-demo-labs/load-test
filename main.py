ff15351c1701bed8cf2e323bc50e7eb15e15afd5
Stuart Sessions <ssessions@veracode.com>
2024-08-01 16:42:26 -0400

    envSetup

---
+++ main.py
@@ -0,0 +1,8 @@
+from modules import EnvSetup
+# Get Env vars
+id, key = EnvSetup.envSetup()
+print(id, key)
+# print(createUsers(id, key))
+# print(result)
+# RunTests()
+
--- modules/EnvSetup.py
+++ modules/EnvSetup.py
@@ -1,17 +1,22 @@
-# Fetch Credentials
 
+import requests
+from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
 import veracode_api_signing.credentials as vcreds
+
+headers = {"User-Agent": "Python HMAC"}
+api_base = "https://api.veracode.com/"
 
 """
 Setup environment variables ID, Key
 """
 def envSetup(id=None, key=None, profile=None, auth_file=None):
+    
 
     api_key_id = id
     api_key_secret = key
-    vcreds.PROFILE_DEFAULT=profile if profile else None
+    vcreds.PROFILE_DEFAULT=profile if profile else 'personal'
 
     if not (id or key):
-        api_key_id, api_key_secret = vcreds.get_credentials(auth_file)
-    
+        api_key_id, api_key_secret = vcreds.get_credentials()
+ 
     return api_key_id, api_key_secret
